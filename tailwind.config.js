2e8dc4299cefc5bb0dfe4a0efeaa88beddbd9977
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-04-19 14:14:05 +0100

    feat: more font size config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         primary: ['"Segoe UI"'],
       },
       fontSize: {
+        xs: ['10px', '13px'],
+        sm: ['10px', '13px'],
+        lg: ['16px', '21px'],
         xl: ['55px', '73px'],
       },
     },
