944d9c7a1574d6446365fbccd0e699a434561c0c
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-04-20 16:40:45 +0100

    feat: toogle other nav working

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,11 +1,12 @@
 import React, { FunctionComponent } from 'react';
 import Image from 'next/image';
 import { useUIContext } from '../Context';
+import OtherNav from './otherNav';
 
 const Nav: FunctionComponent<any> = (): JSX.Element => {
   const { openSidebar } = useUIContext();
   return (
-    <nav className='flex flex-row items-center justify-between p-[10px] w-screen fixed top-0 left-0 right-0'>
+    <nav className='flex flex-row items-center justify-between p-[10px] w-screen fixed top-0 left-0 right-0 md:hidden'>
       <button
         onClick={openSidebar}
         type='button'
@@ -14,21 +15,7 @@ const Nav: FunctionComponent<any> = (): JSX.Element => {
         &#9776;
       </button>
       <Image src='/svgs/main-logo.svg' alt='main-logo' height={32} width={28} />
-      <div className='relative inline-block'>
-        <button
-          type='button'
-          className='text-[25px] rotate-90 text-center text-secondary outline-none'
-        >
-          &#8230;
-        </button>
-        <div className='w-max absolute z-[10000] right-0 shadow-sideBarShadow p-[10px]'>
-          <ul className='p-0'>
-            <li className='py-[10px]'>Overview</li>
-            <li className='py-[10px]'>Policy</li>
-            <li className='py-[10px]'>Overview</li>
-          </ul>
-        </div>
-      </div>
+      <OtherNav />
     </nav>
   );
 };
---
+++ components/nav/otherNav.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent, useState } from 'react';
+
+const OtherNav: FunctionComponent<any> = (): JSX.Element => {
+  const [isOpenState, setOpenState] = useState<boolean>(false);
+  return (
+    <div className='relative inline-block'>
+      <button
+        type='button'
+        className='text-[25px] rotate-90 text-center text-secondary outline-none'
+        onClick={() => setOpenState(!isOpenState)}
+      >
+        &#8230;
+      </button>
+      <div
+        className={`w-max absolute z-[10000] right-0 shadow-sideBarShadow p-[10px] ${
+          isOpenState ? 'block' : 'hidden'
+        }`}
+      >
+        <ul className='p-0'>
+          <li className='py-[10px]'>Overview</li>
+          <li className='py-[10px]'>Policy</li>
+          <li className='py-[10px]'>Overview</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default OtherNav;
