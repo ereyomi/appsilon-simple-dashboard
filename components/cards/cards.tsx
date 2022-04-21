cfea8d4615fad2f6bd1f738bb2853545fb5001bd
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-04-21 04:18:45 +0100

    cards gradient background added

--- components/cards/cards.tsx
+++ components/cards/cards.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-
+import styles from './Card.module.css';
 type cardT = {
   mainPrice: number;
   policyPrice: number;
@@ -38,7 +38,7 @@ const Cards: FunctionComponent<any> = (): JSX.Element => {
       {cardsData.map((card, index) => (
         <div
           key={index}
-          className='w-full h-[298px] lg:w-[314px] lg:h-[298px] bg-white shadow-cardShadow px-[26px] pt-[8px] pb-[28px]'
+          className='w-full h-auto lg:w-[314px] lg:h-[298px] bg-white shadow-cardShadow px-[26px] pt-[8px] pb-[28px]'
         >
           <div className='relative'>
             <span className='font-normal text-xl text-muteColor'>
@@ -63,6 +63,37 @@ const Cards: FunctionComponent<any> = (): JSX.Element => {
                 <span className='text-[13px] leading-[17px] absolute top-0 ml-[3px] text-muteColor'>
                   $
                 </span>
+              </div>
+            </div>
+            <div className='mt-[8px]'>
+              <div
+                className={`w-full h-[5px] rounded-[5px] ${styles.cardBgGradientPrimary}`}
+              >
+                <div
+                  className={`w-2/6 h-[5px] rounded-[5px] ${styles.cardBgGradientSecondary}`}
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div className='mt-[47px]'>
+            <div className='flex flex-row items-center justify-between'>
+              <p className='text-muteColor text-[17px] leading-[23px]'>Claim</p>
+              <div className='relative'>
+                <span className='font-normal text-[22px] leading-[29px] text-muteColor'>
+                  {card.claimPrice}
+                </span>
+                <span className='text-[13px] leading-[17px] absolute top-0 ml-[3px] text-muteColor'>
+                  $
+                </span>
+              </div>
+            </div>
+            <div className='mt-[8px]'>
+              <div
+                className={`w-full h-[5px] rounded-[5px] ${styles.cardBgGradientPrimary}`}
+              >
+                <div
+                  className={`w-2/6 h-[5px] rounded-[5px] ${styles.cardBgGradientTertiary}`}
+                ></div>
               </div>
             </div>
           </div>
